eca7e5595b2c8a5aa9b77e89060a0905f5041e9b
kenjinezumi <kenji.tsuchiya@ovo.com>
2024-05-23 19:55:31 +0100

    Change the server in main for the watcher

--- amplify-watcher/watcher.go
+++ amplify-watcher/watcher.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"log"
+	"net/http"
 	"os"
 
 	"google.golang.org/api/drive/v3"
@@ -22,7 +23,7 @@ func main() {
 	// Get configuration from environment variables
 	folderID := os.Getenv("DRIVE_FOLDER_ID") // Unique folder identifier
 	topic := os.Getenv("PUBSUB_TOPIC")       // Pubsub topic
-	channelID := os.Getenv("CHANNEL_ID")     // Channel id for notifications in case there's another one
+	channelID := os.Getenv("CHANNEL_ID")     // Channel id for notifications
 	webhookURL := os.Getenv("WEBHOOK_URL")   // Cloud function URL
 
 	// Check if all required environment variables are set
@@ -45,4 +46,17 @@ func main() {
 	}
 
 	fmt.Println("Watch set up successfully")
+
+	// Start HTTP server for health checks and to meet Cloud Run requirements
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, "Watcher is active and running.")
+	})
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080" // Default port in local environment if not set
+	}
+	log.Printf("Listening on port %s", port)
+	if err := http.ListenAndServe(":"+port, nil); err != nil {
+		log.Fatalf("Failed to listen and serve: %v", err)
+	}
 }
